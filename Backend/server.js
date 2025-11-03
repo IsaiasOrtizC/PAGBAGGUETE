1495f1dadf41ac412bc65636f5079ceaa534b5c1
BellCampa <isaias.abel@hotmail.com>
2025-11-03 17:01:33 -0300

    ASFASFASF

--- Backend/server.js
+++ Backend/server.js
@@ -2,14 +2,21 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const result = dotenv.config();
+const dotenv = require('dotenv');
 
-dotenv.config();
+if (result.error) {
+    console.error('❌ Error cargando .env:', result.error);
+} else {
+    console.log('✅ Variables de entorno cargadas:', result.parsed);
+}
+console.log('MONGO_URI =', process.env.MONGO_URI);
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Conexión a MongoDB
+
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
